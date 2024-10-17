2c08af05dd6552a9be6f2b44bcab6f5473496c71
SyedKumailAbbas <k214834@nu.edu.pk>
2024-10-18 00:01:08 +0500

    removed case studies

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Hero from "./components/Hero/Hero";
 import Container from "./components/Container/Container";
-import CaseStudies from "./components/Case Studies/CaseStudies";
 import Testimonials from './components/Testimonials/Testimonials';
 import Advisory from './components/Advisory/Advisory';
 import AnimatedBg from './components/AnimatedBg/AnimatedBg'
@@ -14,7 +13,6 @@ export default function Home() {
     <Testimonials />
      <Container /> 
      <BespokeSection/>
-    <CaseStudies />
     <Advisory />  
    </div>
   );
